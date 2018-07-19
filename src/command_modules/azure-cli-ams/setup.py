fd1e0b26b12326413b55ba759ba2df701cd45950
damatojuan <juanignaciodamato@gmail.com>
2018-07-19 19:05:42 -0300

    Update ams sdk version to 1.0.0rc2

--- src/command_modules/azure-cli-ams/setup.py
+++ src/command_modules/azure-cli-ams/setup.py
@@ -33,7 +33,7 @@ CLASSIFIERS = [
 
 DEPENDENCIES = [
     'azure-cli-core',
-    'azure-mgmt-media==1.0.0rc1'
+    'azure-mgmt-media==1.0.0rc2'
 ]
 
 with open('README.rst', 'r', encoding='utf-8') as f:
