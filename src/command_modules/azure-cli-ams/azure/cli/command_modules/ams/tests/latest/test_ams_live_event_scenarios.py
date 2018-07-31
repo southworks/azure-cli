668a1962f9db591167e2317925b1be89419f095b
Nahuel Berg <nahuel.berg@southworks.com>
2018-07-31 16:45:16 -0300

    Partial commit.

--- src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/tests/latest/test_ams_live_event_scenarios.py
+++ src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/tests/latest/test_ams_live_event_scenarios.py
@@ -28,12 +28,13 @@ class AmsLiveEventTests(ScenarioTest):
             self.check('location', 'West US 2')
         ])
 
-        self.cmd('az ams live event create -a {amsname} -l {location} -n {liveEventName} -g {rg} --streaming-protocol {streaming_protocol} --auto-start --encoding-type {encodingType} --preset-name Default720p --tags clave=valor', checks=[
+        self.cmd('az ams live event create -a {amsname} -l {location} -n {liveEventName} -g {rg} --streaming-protocol {streaming_protocol} --auto-start --encoding-type {encodingType} --preset-name Default720p --tags key=value', checks=[
             self.check('name', '{liveEventName}'),
             self.check('location', 'West US 2'),
             self.check('input.streamingProtocol', 'FragmentedMP4'),
             self.check('encoding.encodingType', 'Basic'),
-            self.check('resourceState', 'Running')
+            self.check('resourceState', 'Running'),
+            self.check('presetName', 'Default720p')
         ])
 
     @ResourceGroupPreparer()
@@ -56,18 +57,49 @@ class AmsLiveEventTests(ScenarioTest):
             self.check('location', 'West US 2')
         ])
 
-        self.cmd('az ams live event create -a {amsname} -l {location} -n {liveEventName} -g {rg} --streaming-protocol {streaming_protocol} --encoding-type {encodingType} --preset-name Default720p --tags clave=valor', checks=[
+        self.cmd('az ams live event create -a {amsname} -l {location} -n {liveEventName} -g {rg} --streaming-protocol {streaming_protocol} --encoding-type {encodingType} --preset-name Default720p --tags key=value', checks=[
             self.check('name', '{liveEventName}'),
             self.check('location', 'West US 2'),
             self.check('input.streamingProtocol', 'FragmentedMP4'),
             self.check('encoding.encodingType', 'Basic')
         ])
 
-        self.cmd('az ams live event start -a {amsname} --name {liveEventName} -g {rg}')
-
-        self.cmd('az ams live event show -a {amsname} -n {liveEventName} -g {rg}', checks=[
+        self.cmd('az ams live event start -a {amsname} --name {liveEventName} -g {rg}', checks=[
             self.check('name', '{liveEventName}'),
             self.check('location', 'West US 2'),
             self.check('input.streamingProtocol', 'FragmentedMP4'),
+            self.check('resourceState', 'Starting')
+        ])
+
+    @ResourceGroupPreparer()
+    @StorageAccountPreparer(parameter_name='storage_account_for_create')
+    def test_live_event_stop(self, storage_account_for_create):
+        amsname = self.create_random_name(prefix='ams', length=12)
+        live_event_name = self.create_random_name(prefix='le', length=12)
+
+        self.kwargs.update({
+            'amsname': amsname,
+            'storageAccount': storage_account_for_create,
+            'location': 'westus2',
+            'streaming_protocol': 'FragmentedMP4',
+            'liveEventName': live_event_name,
+            'encodingType': 'Basic'
+        })
+
+        self.cmd('az ams account create -n {amsname} -g {rg} --storage-account {storageAccount} -l {location}', checks=[
+            self.check('name', '{amsname}'),
+            self.check('location', 'West US 2')
+        ])
+
+        self.cmd('az ams live event create -a {amsname} -l {location} -n {liveEventName} -g {rg} --streaming-protocol {streaming_protocol} --encoding-type {encodingType} --preset-name Default720p --tags key=value --auto-start', checks=[
+            self.check('name', '{liveEventName}'),
+            self.check('location', 'West US 2'),
+            self.check('input.streamingProtocol', 'FragmentedMP4'),
+            self.check('encoding.encodingType', 'Basic'),
             self.check('resourceState', 'Running')
+        ])
+
+        self.cmd('az ams live event stop -a {amsname} -n {liveEventName} -g {rg}', checks=[
+            self.check('name', '{liveEventName}'),
+            self.check('resourceState', 'Stopping')
         ])
