d5a5039c6a2d535454e414eeca74f73b99d10316
Fernando Besteiro <fernando.besteiro@southworks.com>
2018-09-20 14:14:54 -0300

    Content keys finished implementation - ERROR 500

--- src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/operations/streaming_locator.py
+++ src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/operations/streaming_locator.py
@@ -3,6 +3,12 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+import json
+
+from knack.util import CLIError
+
+from azure.mgmt.media.models import (StreamingLocatorContentKey, TrackSelection,
+                                     TrackPropertyCondition)
 
 def create_streaming_locator(client, resource_group_name, account_name,
                              streaming_locator_name, streaming_policy_name,
@@ -10,6 +16,11 @@ def create_streaming_locator(client, resource_group_name, account_name,
                              end_time=None, streaming_locator_id=None, alternative_media_id=None,
                              content_keys=None):
     from azure.mgmt.media.models import StreamingLocator
+
+    if not _valid_content_keys(content_keys):
+        raise CLIError('Malformed content keys.')
+
+    content_keys = _build_content_keys(content_keys)
 
     streaming_locator = StreamingLocator(asset_name=asset_name,
                                          start_time=start_time, end_time=end_time,
@@ -26,3 +37,52 @@ def list_content_keys(client, resource_group_name, account_name,
                       streaming_locator_name):
     return client.list_content_keys(resource_group_name, account_name,
                                     streaming_locator_name).content_keys
+
+def _build_content_keys(content_keys):
+    def __track_selection_builder(sel):
+        return TrackPropertyCondition(
+            property=sel.get('property'),
+            operation=sel.get('operation'),
+            value=sel.get('value')
+        )
+
+    def __track_builder(tracks):
+        return TrackSelection(
+            track_selections=[__track_selection_builder(t) 
+                              for t in tracks.get('trackSelections')]
+                              if tracks.get('trackSelections') else None)
+
+    def __content_key_builder(key):
+        return StreamingLocatorContentKey(
+            id=key.get('id'),
+            type=key.get('type'),
+            label=key.get('label'),
+            value=key.get('value'),
+            tracks=[__track_builder(t) for t in key.get('tracks')] if key.get('tracks') else None
+        )
+
+    return [__content_key_builder(k) for k in json.loads(content_keys)]
+
+def _valid_content_keys(content_keys):
+    if content_keys is None:
+        return True
+
+    def __valid_property_conditions(conds):
+        return (conds.get('property') in ['Unknown', 'FourCC'] and
+                conds.get('operation') in ['Unknown', 'Equal'])
+
+    def __valid_track(track):
+        return (track.get('trackSelections') is None or
+                all(__valid_property_conditions(s) for s in track.get('trackSelections')))
+
+    def __valid_content_key(key):
+        return (key.get('id') and
+                (key.get('tracks') is None or all(__valid_track(t) for t in key.get('tracks'))))
+
+    obj = None
+    try:
+        obj = json.loads(content_keys)
+    except ValueError as err:
+        raise CLIError('Malformed JSON: ' + str(err))
+
+    return isinstance(obj, list) and all(__valid_content_key(k) for k in obj)
