a0879eba3c40ac2c925601bb63199934cb7f141f
Fernando Besteiro <fernando.besteiro@southworks.com>
2018-09-26 14:55:46 -0300

    Minor changes in builder

--- src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/operations/streaming_locator.py
+++ src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/operations/streaming_locator.py
@@ -41,27 +41,11 @@ def list_content_keys(client, resource_group_name, account_name,
 
 
 def _build_content_keys(content_keys):
-    def __track_selection_builder(sel):
-        return TrackPropertyCondition(
-            property=sel.get('property'),
-            operation=sel.get('operation'),
-            value=sel.get('value')
-        )
-
-    def __track_builder(tracks):
-        return TrackSelection(
-            track_selections=[None if t is None else __track_selection_builder(t)
-                              for t in tracks.get('trackSelections')]
-            if tracks.get('trackSelections') else None
-        )
-
     def __content_key_builder(key):
         return StreamingLocatorContentKey(
             id=key.get('id'),
-            label=key.get('label'),
-            value=key.get('value'),
-            tracks=[None if t is None else __track_builder(t) for t in key.get('tracks')]
-            if key.get('tracks') else None
+            label_reference_in_streaming_policy=key.get('labelReferenceInStreamingPolicy'),
+            value=key.get('value')
         )
 
     return None if content_keys is None else [__content_key_builder(k) for k in json.loads(content_keys)]
@@ -71,19 +55,11 @@ def _valid_content_keys(content_keys):
     if content_keys is None:
         return True
 
-    def __valid_property_conditions(conds):
-        return (conds.get('property') in ['Unknown', 'FourCC'] and
-                conds.get('operation') in ['Unknown', 'Equal'])
-
-    def __valid_track(track):
-        return (track.get('trackSelections') is None or
-                all(__valid_property_conditions(s) for s in track.get('trackSelections')))
-
     def __valid_content_key(key):
-        return (key.get('id') and
-                (key.get('tracks') is None or all(__valid_track(t) for t in key.get('tracks'))))
+        return key.get('id') and key.get('value') and key.get('labelReferenceInStreamingPolicy')
 
     obj = None
+
     try:
         obj = json.loads(content_keys)
     except ValueError as err:
