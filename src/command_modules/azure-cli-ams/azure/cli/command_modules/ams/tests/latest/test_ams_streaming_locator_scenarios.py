4c013df7147093fb9be4defa5683dc03f99ed8b2
Renkon <fernando.besteiro@southworks.com>
2018-09-27 13:00:01 -0300

    Preparing test. Added content keys to test. Missing merge to work successfully

--- src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/tests/latest/test_ams_streaming_locator_scenarios.py
+++ src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/tests/latest/test_ams_streaming_locator_scenarios.py
@@ -5,8 +5,14 @@
 
 from azure.cli.testsdk import ScenarioTest, ResourceGroupPreparer, StorageAccountPreparer
 
+import os
 
 class AmsStreamingLocatorTests(ScenarioTest):
+    def _get_test_data_file(self, filename):
+        filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', filename)
+        self.assertTrue(os.path.isfile(filepath), 'File {} does not exist.'.format(filepath))
+        return filepath
+
     @ResourceGroupPreparer()
     @StorageAccountPreparer(parameter_name='storage_account_for_create')
     def test_ams_streaming_locator(self, resource_group, storage_account_for_create):
@@ -64,6 +70,66 @@ class AmsStreamingLocatorTests(ScenarioTest):
         self.cmd('az ams streaming-locator get-paths -a {amsname} -n {streamingLocatorName} -g {rg}')
 
         self.cmd('az ams streaming-locator delete -n {streamingLocatorName} -a {amsname} -g {rg}')
+
+    @ResourceGroupPreparer()
+    @StorageAccountPreparer(parameter_name='storage_account_for_create')
+    def test_ams_streaming_locator_with_content_keys(self, resource_group, storage_account_for_create):
+        amsname = self.create_random_name(prefix='ams', length=12)
+
+        self.kwargs.update({
+            'amsname': amsname,
+            'storageAccount': storage_account_for_create,
+            'location': 'westus2'
+        })
+
+        self.cmd('az ams account create -n {amsname} -g {rg} --storage-account {storageAccount} -l {location}')
+
+        asset_name = self.create_random_name(prefix='asset', length=12)
+
+        self.kwargs.update({
+            'assetName': asset_name
+        })
+
+        self.cmd('az ams asset create -a {amsname} -n {assetName} -g {rg}')
+
+        streamingPolicyName = self.create_random_name(prefix='spn', length=10)
+
+        self.kwargs.update({
+            'streamingPolicyName': streamingPolicyName
+        })
+
+        self.cmd('az ams streaming-policy create -a {amsname} -n {streamingPolicyName} -g {rg} --download')
+
+        streaming_locator_name = self.create_random_name(prefix='sln', length=10)
+
+        self.kwargs.update({
+            'streamingLocatorName': streaming_locator_name,
+            'startTime': '2018-03-29T10:00:00',
+            'endTime': '2018-03-29T12:00:00',
+            'streamingLocatorId': self.create_guid(),
+            'alternativeMediaId': self.create_guid(),
+            'contentKeys': '@' + self._get_test_data_file('contentKeys.json')
+        })
+
+        self.cmd('az ams streaming-locator create -n {streamingLocatorName} -a {amsname} -g {rg} --streaming-policy-name {streamingPolicyName} --asset-name {assetName} --start-time {startTime} --end-time {endTime} --streaming-locator-id {streamingLocatorId} --alternative-media-id {alternativeMediaId} --content-keys "{contentKeys}"', checks=[
+            self.check('name', '{streamingLocatorName}'),
+            self.check('resourceGroup', '{rg}'),
+            self.check('streamingLocatorId', '{streamingLocatorId}'),
+            self.check('alternativeMediaId', '{alternativeMediaId}')
+        ])
+
+        self.cmd('az ams streaming-locator show -a {amsname} -n {streamingLocatorName} -g {rg}', checks=[
+            self.check('name', '{streamingLocatorName}'),
+            self.check('resourceGroup', '{rg}')
+        ])
+
+        list = self.cmd('az ams streaming-locator list -a {amsname} -g {rg}').get_output_in_json()
+        assert len(list) > 0
+
+        self.cmd('az ams streaming-locator get-paths -a {amsname} -n {streamingLocatorName} -g {rg}')
+
+        self.cmd('az ams streaming-locator delete -n {streamingLocatorName} -a {amsname} -g {rg}')
+
 
     @ResourceGroupPreparer()
     @StorageAccountPreparer(parameter_name='storage_account_for_create')
