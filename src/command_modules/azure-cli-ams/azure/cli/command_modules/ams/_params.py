5c8769a93ce0ebe95101003cb637418bdb7b5da3
Renkon <fernando.besteiro@southworks.com>
2018-10-11 11:37:01 -0300

    Implemented AllowAll on LiveEvent

--- src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/_params.py
+++ src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/_params.py
@@ -337,8 +337,8 @@ def load_arguments(self, _):  # pylint: disable=too-many-locals, too-many-statem
                    help='ISO 8601 timespan duration of the key frame interval duration.')
         c.argument('access_token', arg_group='Input', help='A unique identifier for a stream. This can be specified at creation time but cannot be updated. If omitted, the service will generate a unique value.')
         c.argument('description', help='The live event description.')
-        c.argument('ips', ip_range_type, arg_group='Input')
-        c.argument('preview_ips', ip_range_type, arg_group='Preview')
+        c.argument('ips', ip_range_type, arg_group='Input', help='Space-separated IP addresses for access control. Allowed IP addresses can be specified as either a single IP address (e.g. "10.0.0.1") or as an IP range using an IP address and a CIDR subnet mask (e.g. "10.0.0.1/22"). Use "" to clear existing list. Use "AllowAll" to allow all IP addresses. Allowing all IPs is not recommended for production environments.')
+        c.argument('preview_ips', ip_range_type, arg_group='Preview', help='Space-separated IP addresses for access control. Allowed IP addresses can be specified as either a single IP address (e.g. "10.0.0.1") or as an IP range using an IP address and a CIDR subnet mask (e.g. "10.0.0.1/22"). Use "" to clear existing list. Use "AllowAll" to allow all IP addresses.')
         c.argument('preview_locator', arg_group='Preview', help='The identifier of the preview locator in Guid format. Specifying this at creation time allows the caller to know the preview locator url before the event is created. If omitted, the service will generate a random identifier. This value cannot be updated once the live event is created.')
         c.argument('streaming_policy_name', arg_group='Preview', help='The name of streaming policy used for the live event preview. This can be specified at creation time but cannot be updated.')
         c.argument('alternative_media_id', arg_group='Preview', help='An Alternative Media Identifier associated with the StreamingLocator created for the preview. This value is specified at creation time and cannot be updated. The identifier can be used in the CustomLicenseAcquisitionUrlTemplate or the CustomKeyAcquisitionUrlTemplate of the StreamingPolicy specified in the StreamingPolicyName field.')
@@ -349,8 +349,8 @@ def load_arguments(self, _):  # pylint: disable=too-many-locals, too-many-statem
 
     with self.argument_context('ams live-event update') as c:
         c.argument('description', help='The live event description.')
-        c.argument('ips', ip_range_type, arg_group='Input')
-        c.argument('preview_ips', ip_range_type, arg_group='Preview')
+        c.argument('ips', ip_range_type, arg_group='Input', help='Space-separated IP addresses for access control. Allowed IP addresses can be specified as either a single IP address (e.g. "10.0.0.1") or as an IP range using an IP address and a CIDR subnet mask (e.g. "10.0.0.1/22"). Use "" to clear existing list. Use "AllowAll" to allow all IP addresses. Allowing all IPs is not recommended for production environments.')
+        c.argument('preview_ips', ip_range_type, arg_group='Preview', help='Space-separated IP addresses for access control. Allowed IP addresses can be specified as either a single IP address (e.g. "10.0.0.1") or as an IP range using an IP address and a CIDR subnet mask (e.g. "10.0.0.1/22"). Use "" to clear existing list. Use "AllowAll" to allow all IP addresses.')
         c.argument('tags', arg_type=tags_type)
         c.argument('client_access_policy', arg_group='Cross Site Access Policies', help='The local full path to the clientaccesspolicy.xml used by Silverlight.')
         c.argument('cross_domain_policy', arg_group='Cross Site Access Policies', help='The local full path to the crossdomain.xml used by Silverlight.')
